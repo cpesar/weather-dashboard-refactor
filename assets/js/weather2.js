0410b035b5f9828c01449ebb091640e59d99e709
cpesar <77510555+cpesar@users.noreply.github.com>
2021-04-29 16:42:19 -0600

    code is functioning, need to update css

--- assets/js/weather2.js
+++ assets/js/weather2.js
@@ -39,8 +39,8 @@ function addCurrentData(data, location) {
 function addFiveDayData(data) {
   let fiveDayForecast = '';
   let fiveDayElement = document.getElementById('five-day-forecast');
-  //loops through first 5 days in data.daily array returned from api
-  //`` template strings allow you to write multi lines like below... ${...in here you can add javascript stuff}
+//LOOPS THROUGH FIRST FIVE DAYS IN DATA.DAILY ARRAY
+ 
   for (let i = 0; i < 5; i++) {
     fiveDayForecast += `
       <div class="day-forecast">
@@ -52,6 +52,96 @@ function addFiveDayData(data) {
         <p>Humidity: ${data.daily[i].humidity}</p>
       </div>
     `;
+      //`` TEMPLATE LITERAL STRINGS USE ${____} AND ALLOW YOU TO WRITE LESS CODE
   }
   fiveDayElement.innerHTML = fiveDayForecast;
+}
+
+
+
+
+//REACHES OUT TO APIS AND SETS WEATHER DATA
+
+//initialLocation parameter is a optional param...look up default javascript parameters...only time function gets called without argument passed in is the input search
+async function submit(initialLocation = null) {
+  var location = document.getElementById('cityState').value;
+  //checks if input has value
+  if (initialLocation) {
+    location = initialLocation;
+  }
+  var encodedLocation = encodeURIComponent(location);
+  var googleApiKey = 'AIzaSyD3inT5sV9fa3A2gXTv4gnOQ8UjoUVEG44';
+  var weatherApiKey = '9f7f519c99b1d80f760dfa0c24619d7d';
+  var latLong;
+  var spinner = document.getElementById('spinner');
+
+  //show spinner on data load
+  spinner.classList.remove('hide');
+
+  //if searchHistory array doesn't already include city, state then add to the list
+  if (location && !searchHistory.includes(location)) {
+    addCityToSearchHistory(location);
+  }
+
+  if (location) {
+    await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedLocation}&key=${googleApiKey}`
+    )
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        latLong = data.results[0].geometry.location;
+        console.log('latlong data: ', latLong);
+      });
+
+    await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latLong.lat}&lon=${latLong.lng}&appid=${weatherApiKey}`
+    )
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        console.log('weather data: ', data);
+        addCurrentData(data, location);
+        addFiveDayData(data);
+        spinner.classList.add('hide');
+      });
+  }
+}
+
+// GENERATES HTML FOR SEARCH HISTORY LIST
+function htmlSearchHistoryGenerator(history) {
+  var historyElement = document.getElementById('search-history');
+  var historyList = '';
+  for (let i = 0; i < history.length; i++) {
+    historyList += `<li onclick="submit('${history[i]}')">${history[i]}</li>`;
+  }
+  historyElement.innerHTML = historyList;
+}
+
+
+
+//CHECKS LOCAL STORAGE FOR SEARCH HISTORY AND SETS INITAL DATA IF HISTORY EXISITS ON PAGE LOAD
+//SETS TO FIRST ITEM IN THE ARRAY(THE LAST SEARCH)
+function checkSearchHistory() {
+  let history = JSON.parse(localStorage.getItem('weatherSearchHistory'));
+  if (history) {
+    searchHistory = history;
+    htmlSearchHistoryGenerator(searchHistory);
+    submit(searchHistory[0]);
+  }
+}
+
+
+//ADDS CITY TO SEARCH HISTORY IN LOCAL STORAGE, IF 5 CITIES ARE ALREADY THERE- LAST ITEM IN ARRAY WILL BE REMOVED VIA POP()
+//ADDS MOST RECENT SEARCH TO THE FRONT OF THE ARRAY USING UNSHIFT()
+
+function addCityToSearchHistory(city) {
+  if (searchHistory.length >= 5) {
+    searchHistory.pop();
+  }
+  searchHistory.unshift(city);
+  localStorage.setItem('weatherSearchHistory', JSON.stringify(searchHistory));
+  htmlSearchHistoryGenerator(searchHistory);
 }
